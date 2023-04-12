36b3723fe1a3326941b7576405da33a2bd5a4139
Noman Ahmed <noman3085386@gmail.com>
2023-04-12 09:01:17 +0500

    Update payment.tsx

--- src/pages/components/payment.tsx
+++ src/pages/components/payment.tsx
@@ -5,12 +5,12 @@ export default function payment() {
   <h1
   className="absolute h-[69px] w-[600px] left-[50px] right-[198px] top-[1269px] font-bold text-[50px] leading-[68px] tracking-[-0.3px] text-[#262626;]"
   >Choose a way to pay</h1>
-  <div className="border-box absolute w-[315px] h-[100px] left-[50px] top-[1388px]  bg-[#FFFFFF] border-2 border-solid border-gray-900 rounded-lg
+  <button className="border-box absolute w-[315px] h-[100px] left-[50px] top-[1388px]  bg-[#FFFFFF] border-2 border-solid border-gray-900 rounded-lg
 
 
 
-"><span className="absolute w-[70px] h-[41ps] left-[122px] top-[30px] font-noto text-[30px] leading-[41px] tracking-[-0.3px] text-[#262626]">Cash</span></div>
- <div className="absolute w-[315px] h-[100px] left-[385px] top-[1388px] bg-[#EDF2F7]  ">
+"><span className="absolute w-[70px] h-[41ps] left-[122px] top-[30px] font-noto text-[30px] leading-[41px] tracking-[-0.3px] text-[#262626]">Cash</span></button>
+ <button className="absolute w-[315px] h-[100px] left-[385px] top-[1388px] bg-[#EDF2F7]  ">
     
     <span className="absolute w-[158px] h-[41px] left-[78px] top-[30px] font-noto font-semibold text-[30px] leading-[41px] text-center  tracking-[-0.3px] text-[#262626]">
 
@@ -18,8 +18,8 @@ export default function payment() {
  </span>
  
  
- </div>
- <div className="absolute w-[315px] h-[100px] left-[50px] top-[1506px] bg-[#EDF2F7]  ">
+ </button>
+ <button className="absolute w-[315px] h-[100px] left-[50px] top-[1506px] bg-[#EDF2F7]  ">
     
     <span className="absolute w-[200px] h-[41px] left-[55px] top-[15px] font-noto font-semibold text-[30px] leading-[41px] text-center  tracking-[-0.3px] text-[#262626]">
 
@@ -27,7 +27,7 @@ export default function payment() {
 Virtual bank </span>
  
  
- </div>
+ </button>
  
   </>
   )
